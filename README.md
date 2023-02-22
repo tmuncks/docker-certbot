1845b19f2cc61cdc64d946404c61ddcd0a995caa
Thomas M Steenholdt <tmus@tmus.dk>
2023-02-22 00:50:21 -0300

    add environment options for dns-powerdns configuration and update the docs/examples

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-Certbot with certbot-dns-powerdns plugin that supports PowerDNS-Admin
+## certbot docker container
+
+With certbot-dns-powerdns plugin that supports PowerDNS-Admin
+
+### How it works
+
+Parameters to `certbot` can be passed as the command to the container, just like the original `certbot/certbot` image.
+
+### Oneshot mode
+
+The default is to run `certbot` once and exit. This **oneshot** mode works exactly like the original `certbot/certbot` image.
+
+### Automatic renewal
+
+Sometimes it is useful to leave the container running after certificate generation, and automatically handle renewals on regular intervals as well.
+
+We can disable **oneshot** mode by setting the `CERTBOT_ONESHOT` environment variable to `0`.
+
+This will still cause the image to run `certbot` on start, but it will not exit. Instead, it will repeat the `certbot` command every night at 03:00, indefinitely.
+
+### PowerDNS integration
+
+Parameters to the dns-powerdns authenticator plugin, can be specified by
+setting the `DNS_POWERDNS_API_URL` and `DNS_POWERDNS_API_KEY` environment
+variables.
+
+They can also be passed as a mounted `pdns-credentials.ini` file,
+containing the `--dns-powerdns-api-url` and `--dns-powerdns-api-key` options,
+as documented for [certbot-dns-powerdns](https://github.com/tmuncks/certbot-dns-powerdns).
+
+
+### Environment variables
+
+Environment variables:
+    CERTBOT_ONESHOT             Set to 0 to automatically rerun certbot every
+                                night at 03:00
+    DNS_POWERDNS_API_URL        Specify value for --dns-powerdns-api-url
+    DNS_POWERDNS_API_KEY        Specify value for --dns-powerdns-api-key
+
+### Examples
+
+An example `docker-compose.yml` can be found under the `compose` directory.
--- docker/src/main.py
+++ docker/src/main.py
@@ -11,9 +11,15 @@ It is possible to disable this `oneshot` operation, to have the container
 auto-renew certificates nightly at 03:00. This is achieved by setting the
 `CERTBOT_ONESHOT` environment variable to `0`.
 
+Parameters to the dns-powerdns authenticator plugin, can be specified by
+setting the `DNS_POWERDNS_API_URL` and `DNS_POWERDNS_API_KEY` environment
+variables
+
 Environment variables:
-  CERTBOT_ONESHOT           Set to 0 to automatically rerun certbot every
-                            night at 03:00
+    CERTBOT_ONESHOT             Set to 0 to automatically rerun certbot every
+                                night at 03:00
+    DNS_POWERDNS_API_URL        Specify value for --dns-powerdns-api-url
+    DNS_POWERDNS_API_KEY        Specify value for --dns-powerdns-api-key
 """
 import sys
 import os
@@ -26,7 +32,7 @@ def do_certbot(argv):
     """
     Takes care of actually running certbot with the proper parameters
     """
-    command = ['certbot'] + list(argv[1:])
+    command = ['./certbot'] + list(argv)
     print(f":: Running command: {' '.join(command)}")
     res = subprocess.run(command)
     print(f":: {command[0]}: {res}")
@@ -36,16 +42,32 @@ def main(argv):
     """
     Main loop
     """
-    do_certbot(argv)
-    if os.getenv('CERTBOT_ONESHOT', '1') == '0':
-        while True:
-            now = datetime.datetime.now()
-            renew = now + datetime.timedelta(days=1)
-            renew = renew.replace(hour=3, minute=0, second=0, microsecond=0)
-            print(f":: Sleeping until: {renew}")
-            time.sleep((renew - now).total_seconds())
-            do_certbot(argv)
+    # handle options
+    # any changes to argv is done at the start of the list, to make the generated changes
+    # overrideable by command line args
+    api_url = os.getenv('DNS_POWERDNS_API_URL')
+    api_key = os.getenv('DNS_POWERDNS_API_KEY')
+    if api_url and api_key:
+        with open("/pdns-credentials-generated.ini", "wt") as f:
+            f.write(f"dns_powerdns_api_url = {api_url}\n")
+            f.write(f"dns_powerdns_api_key = {api_key}\n")
+        argv = ["--dns-powerdns-credentials", "/pdns-credentials-generated.ini"] + argv
+        argv = ["--authenticator", "dns-powerdns"] + argv
+
+    # actual certbot loop
+    while True:
+        do_certbot(argv)
+        # exit if oneshot operation
+        if os.getenv('CERTBOT_ONESHOT', '1') != '0':
+            break
+        # sleep until 03:00
+        now = datetime.datetime.now()
+        renew = now + datetime.timedelta(days=1)
+        renew = renew.replace(hour=3, minute=0, second=0, microsecond=0)
+        print(f":: Sleeping until: {renew}")
+        time.sleep((renew - now).total_seconds())
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    # strip the script name off the args list, then pass to main loop
+    main(sys.argv[1:])
